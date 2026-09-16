24d6e7ccaed2f292aab4c4fbe98ab61d10a0470c
jerryjohnsoffl <118886936+jerryjohnsoffl@users.noreply.github.com>
2026-09-16 12:30:53 +0530

    Social Icons added

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Hero from './Hero/Hero'
 import About from './About/About'
 import Skills from './Skills/Skills'
 import Education from './Education/Education'
+import Contact from './Contact/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,6 +21,7 @@ function App() {
         <About />
         <Skills />
         <Education />
+        <Contact />
       </div>
     </>
   )
